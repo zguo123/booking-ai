46aa330c4fbf9aaf04ac0b75d04c9f8fe3808329
Zhaoyu <zhaoyu.guo@mail.thecustomercompany.ca>
2023-06-18 03:33:56 -0400

    Fixed dark mode

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,25 +5,37 @@ import { SaasProvider } from "@saas-ui/react";
 
 import theme from "@/lib/theme";
 import { Inter, Open_Sans, Raleway } from "next/font/google";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { theme as proTheme } from "@saas-ui/pro";
 
 const inter = Inter({ subsets: ["latin"] });
 const raleway = Raleway({ subsets: ["latin"] });
-const openSans = Open_Sans({ subsets: ["latin"] });
+
+const BaseTheme = extendTheme(
+  {
+    fonts: {
+      heading: `${raleway.style.fontFamily}, sans-serif`,
+      body: `${inter.style.fontFamily}, sans-serif`,
+    },
+
+    config: {
+      initialColorMode: "system",
+      useSystemColorMode: true,
+    },
+  },
+  theme
+);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <style jsx global>
-        {`
-          :root {
-            --font-inter: ${inter.style.fontFamily};
-            --font-raleway: ${raleway.style.fontFamily};
-            --font-open-sans: ${openSans.style.fontFamily};
-          }
-        `}
-      </style>
-      <CacheProvider>
-        <SaasProvider theme={theme}>{children}</SaasProvider>
+      <CacheProvider speedy>
+        <SaasProvider resetCSS theme={BaseTheme}>
+          <ColorModeScript
+            initialColorMode={BaseTheme.config.initialColorMode}
+          />
+          {children}
+        </SaasProvider>
       </CacheProvider>
     </>
   );
