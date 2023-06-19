62cafa2aaebbca05fe32033341f8df122da01dc4
Zhaoyu <zhaoyu.guo@mail.thecustomercompany.ca>
2023-06-19 16:50:12 -0400

    Fixed logging issue

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,9 +1,17 @@
 import pino from "pino";
-export default pino({
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    },
-  },
+import pretty from "pino-pretty";
+
+const stream = pretty({
+  levelFirst: true,
+  colorize: true,
+  ignore: "time,hostname",
 });
+
+const logger = pino(
+  {
+    level: process.env.NODE_ENV === "development" ? "debug" : "info",
+  },
+  stream
+);
+
+export default logger;
