043c741b174b793672237ec352ea1538f9263d7c
Zhaoyu <zhaoyu.guo@mail.thecustomercompany.ca>
2023-06-23 10:11:17 -0400

    Fixed type error

--- src/components/PageContent/Dashboard/DashboardIntegrationPage.tsx
+++ src/components/PageContent/Dashboard/DashboardIntegrationPage.tsx
@@ -1,40 +1,7 @@
 "use client";
 
-import {
-  appointmentData,
-  appointmentDataColumns,
-} from "@/lib/consts/appointments";
-import { formatPrice } from "@/lib/helpers/appointment";
-import { SettingsIcon } from "@chakra-ui/icons";
-import { Button } from "@chakra-ui/react";
-import { DataGrid, DataGridPagination } from "@saas-ui/pro";
-
 export default function DashboardIntegrationPage() {
   // convert the date to a string
 
-  return (
-    <DataGrid
-      isHoverable
-      isSelectable
-      isSortable
-      columns={[
-        ...appointmentDataColumns,
-        {
-          id: "actions",
-          header: "More Actions",
-
-          cell: () => <Button leftIcon={<SettingsIcon />}>Manage</Button>,
-        },
-      ]}
-      data={appointmentData.map((appointment) => {
-        return {
-          ...appointment,
-          appointmentDate: appointment.appointmentDate.toDateString(),
-          totalPrice: formatPrice(appointment.totalPrice),
-        };
-      })}
-    >
-      <DataGridPagination />
-    </DataGrid>
-  );
+  return <></>;
 }
